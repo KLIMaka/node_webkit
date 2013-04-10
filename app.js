58d20539ede869acdb81229fabe4c43ca727d13d
U-User-PC\User <User@User-PC.(none)>
2013-04-10 12:16:35 +0300

    stl && list

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-requirejs(['module1'], function(foo){
+requirejs(['module1', 'stl', 'sentinellist'], function(foo, stl, List){
 
-	alert(foo());
+	function println(val) {
+		window.document.write(val + '<br>');
+	}
+
+	var a = [1,2,3,4,5];
+	var b = new List();
+
+	stl.copy     (a.begin(), a.end(), new stl.Inserter(b));
+	stl.transform(b.begin(), b.end(), function(val) { return val + 11 });
+
+	println(b.toString());
 })
---
+++ stl.js
@@ -0,0 +1,116 @@
+define(function(){
+
+var exports = {};
+
+// Iterators
+
+exports.ArrayIterator = function(array, idx) {
+	this.array = array;
+	this.idx = idx == undefined ? 0 : idx;
+};
+
+exports.ArrayIterator.prototype = {
+
+	clone : function() {
+		return new exports.ArrayIterator(this.array, this.idx);
+	},
+
+	equals : function(iter) {
+		return this.array === iter.array && this.idx === iter.idx;
+	},
+
+	get : function() {
+		return this.array[this.idx];
+	},
+
+	set : function(val) {
+		this.array[this.idx] = val;
+	},
+
+	next : function() {
+		return this.array[this.idx++];
+	},
+
+	prev : function() {
+		return this.array[this.idx--];
+	}
+};
+
+exports.Inserter = function(cont, iter) {
+	this.cont = cont;
+	this.iter = iter == undefined ? cont.begin() : iter;
+}
+
+exports.Inserter.prototype = {
+
+	clone : function() {
+		return new exports.Inserter(this.cont, this.iter);
+	},
+
+	equals : function(iter) {
+		return this.cont === iter.cont && this.iter === iter.iter;
+	},
+
+	set : function(val) {
+		this.cont.insert(this.iter, val);
+		this.iter.next();
+	},
+
+	next : function(){},
+}
+
+// Utilites
+
+Array.prototype.begin = function(idx) {
+	return new exports.ArrayIterator(this, idx);
+}
+
+Array.prototype.end = function() {
+	return new exports.ArrayIterator(this, this.length);
+}
+
+Array.prototype.insert = function(iter, val) {
+	this.splice(iter.idx, 0, val);
+}
+
+// Alghoritms
+
+exports.swap = function(i1, i2) {
+	var tmp = i1.get();
+	i1.set(i2.get());
+	i2.set(tmp);
+}
+
+exports.copy = function(ib, ie, ob) {
+	while (!ib.equals(ie)) {
+		ob.set(ib.next());
+		ob.next();
+	}
+	return ob;
+}
+
+exports.transform = function(b, e, func) {
+	while (!b.equals(e)) {
+		b.set(func(b.get()));
+		b.next();
+	}
+}
+
+exports.apply = function(b, e, func) {
+	while (!b.equals(e)) {
+		func(b.next());
+	}
+}
+
+exports.find = function(b, e, val) {
+	while (!b.equals(e)) {
+		if (b.get() == val)
+			return b;
+		b.next();
+	}
+	return b;
+}
+
+return exports;
+
+});
